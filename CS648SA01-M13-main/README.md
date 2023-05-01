bc9a8d003626f9b08c8e12550ca23811fbe01adf
Rutujas1709 <nyrutuja@gmail.com>
2023-04-30 23:25:29 -0700

    GC648_M13_LAB_up and running with react

---
+++ CS648SA01-M13-main/README.md
@@ -0,0 +1,2 @@
+# CS648SA01-M13
+## up and running with react
---
+++ CS648SA01-M13-main/module01/src/employees.jsx
@@ -0,0 +1,21 @@
+let vaibhavi = "Vaibhavi Deo";
+let shamali = "Shamali Shah";
+let shruti = "Shruti Bhosale";
+let sanket = "Sanket Khote";
+let prajwal = "Prajwal Ganeshe";
+let eric = "Eric Ackermann";
+let piyush = "Piyush Jadhav";
+let ritesh = "Ritesh Pehlani";
+const element = (
+  <ul style={{ color: "blue", fontSize: "24px" }}>
+    <li>{vaibhavi}</li>
+    <li>{shamali}</li>
+    <li>{shruti}</li>
+    <li>{sanket}</li>
+    <li>{prajwal}</li>
+    <li>{eric}</li>
+    <li>{piyush}</li>
+    <li>{ritesh}</li>
+  </ul>
+);
+ReactDOM.render(element, document.getElementById("content"));
